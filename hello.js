d4b1c6366c45c73e673c82502dd926ad672cfe1a
Sang Tran <tony_633@hotmail.com>
2017-05-28 22:46:58 +0000

    looping and repition assignment

--- hello.js
+++ hello.js
@@ -1,5 +1,18 @@
-function sayHello(name) {
-  console.log("Hello, world " + name);
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "choco$
+
+// Write a while loop that prints out the contents of ingredients:
+var index = 0;
+while (index != ingredients.length) {
+        console.log(ingredients[index]);
+        index++;
 }
 
-sayHello("Sang");
+// Write a for loop that prints out the contents of ingredients:
+
+for (var i = 0; i < ingredients.length; i++){
+        console.log(ingredients[i]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (var i = ingredients.length; i >= 0; i--) {
+        console.log(ingredients[i]);
+}
